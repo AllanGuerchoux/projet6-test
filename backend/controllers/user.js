0b063be65bf9dc76dbb4b3cdb21746a3923c0ec4
Allan Guerchoux <allan.gcx@gmail.com>
2024-10-25 18:49:15 +0200

    ajout d'express-validator et moddification de middkeware signup

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,23 +1,46 @@
 const bcrypt = require('bcrypt');
+const {body, validationResult } = require('express-validator')
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 exports.signup = (req, res, next) => {
-    if (!req.body.password) {
-        return res.status(400).json({ error: 'Mot de passe requis' });
-    }
+    // Validation et assainissement des données
+    const validationRules = [
+        body('email')
+            .isEmail()
+            .withMessage('Doit être un email valide.')
+            .normalizeEmail(),
+        body('password')
+            .isLength({ min: 8 }) // Minimum de 8 caractères
+            .withMessage('Le mot de passe doit contenir au moins 8 caractères.')
+            .matches(/\d/)
+            .withMessage('Le mot de passe doit contenir au moins un chiffre.')
+            .matches(/[A-Z]/)
+            .withMessage('Le mot de passe doit contenir au moins une lettre majuscule.')
+            .matches(/[!@#$%^&*(),.?":{}|<>]/)
+            .withMessage('Le mot de passe doit contenir au moins un caractère spécial.')
+    ];
+    Promise.all(validationRules.map(validation => validation.run(req)))
+        .then(() => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
 
-    bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-        const user = new User({
-            email: req.body.email,
-            password: hash
-        });
-        user.save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-        .catch(error => res.status(400).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
+            // Hash du mot de passe et création de l'utilisateur
+            bcrypt.hash(req.body.password, 10)
+                .then(hash => {
+                    const user = new User({
+                        email: req.body.email,
+                        password: hash
+                    });
+                    user.save()
+                        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+                        .catch(error => res.status(400).json({ error }));
+                })
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
 };
 
 exports.login = (req, res, next) => {
